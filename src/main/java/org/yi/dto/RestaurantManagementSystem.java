16bbfe9689e8fca949a6eb37a909fc2d2b5da4bd
wangy <andrewtop1986@gmail.com>
2023-11-15 10:10:17 -0500

    Clean code

--- src/main/java/org/yi/dto/RestaurantManagementSystem.java
+++ src/main/java/org/yi/dto/RestaurantManagementSystem.java
@@ -3,9 +3,6 @@ package org.yi.dto;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
-import lombok.ToString;
-
-import java.util.Arrays;
 
 @EqualsAndHashCode
 @Getter
